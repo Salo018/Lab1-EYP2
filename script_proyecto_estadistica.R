7acc50f091c796962a28c93534437a0012609876
juan_programing <juanestebanparedes592@gmail.com>
2026-02-26 21:40:23 -0500

    normalization of qualitative columns

--- script_proyecto_estadistica.R
+++ script_proyecto_estadistica.R
@@ -1,17 +1,20 @@
-install.packages("ggplot2") #Visualización
+# Importar librerias solo si no se han instalado
+install.packages("ggplot2") #Visualizacion
 install.packages("naniar")  #Datos faltantes
 install.packages("corrplot") #Matriz de correlaciones
 install.packages("dplyr")    #Manejo de datos
 
 
-#Se cargan las librerias con library()
+#Se cargan las librerias 
 library(ggplot2)
 library(naniar)
 library(corrplot)
 library(dplyr)
+# Librerias para cargar datos
 library(readr)
 library(purrr)
 library(dplyr)
+library(stringr)
 
 # URL de cada csv
 urls <- c(
@@ -29,13 +32,40 @@ urls <- c(
 
 # Combinar csv en un df
 lista_csv <- map(urls, read_csv)
-datos_finales <- bind_rows(lista_csv)
+df <- bind_rows(lista_csv)
+
+# Ver valores unicos de una columna
+unique(df$genero)
+unique(df$carrera)
+unique(df$acceso_internet)
+unique(df$trabaja)
+unique(df$modalidad)
 
 
+# Normalización de datos de columnas
 
+#Columna genero
+df$genero <- tolower(df$genero) # Connvertir a minusculas los datos
+#unique(df$genero)
 
+# Columna carrera
+df$carrera <- str_replace(df$carrera, "Busines$", "Business") 
+# Se coloca Busines$ para que solo reemplace en la palabra exacta y no queden como Businesss 
+df$carrera <- str_replace(df$carrera, "Ingenieria", "Engineering")
+df$carrera <- str_replace(df$carrera, "CS", "Computer Science")
+#unique(df$carrera)
 
+# Columna acceso_internet
+df$acceso_internet <- tolower(df$acceso_internet) 
 
+# Columna trabaja
+df$trabaja<- tolower(df$trabaja) 
+df$trabaja <- str_replace(df$trabaja, "si", "sí") 
+df$trabaja <- str_replace(df$trabaja, "sí", "yes")
+
+# Columna modalidad
+df$modalidad<- tolower(df$modalidad) 
+df$modalidad <- str_replace(df$modalidad, "presencial", "in_person") 
 
 
 
