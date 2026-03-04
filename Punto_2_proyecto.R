c2aaef0e4af66fb53df655c7e0664eabd4883f29
Salo018 <salome.rivas@uao.edu.co>
2026-03-04 00:39:10 -0500

    Metrics and Pixel matrix

--- Punto_2_proyecto.R
+++ Punto_2_proyecto.R
@@ -32,6 +32,40 @@ R <- image_matrix[,,1] / 255  # canal rojo
 G <- image_matrix[,,2] / 255  # canal verde
 B <- image_matrix[,,3] / 255  # canal azul
 
+# PASO 4: Calcular las metricas del profesor
+# Intensidad en escala de grises
+Igray <- 0.2989*R + 0.5870*G + 0.1140*B
+
+# Brillo medio
+N <- length(Igray)
+brillo_medio <- sum(Igray) / N
+
+# Contraste global
+contraste <- sqrt(sum((Igray - brillo_medio)^2) / (N-1))
+
+# Segmentacion binaria
+B_mask <- ifelse(Igray < 0.95, 1, 0)
+
+# Area del objeto
+area <- sum(B_mask)
+
+# Rugosidad superficial
+Igray_obj <- Igray[B_mask == 1]
+G_obj     <- G[B_mask == 1]
+M         <- length(Igray_obj)
+mu_omega  <- mean(Igray_obj)
+rugosidad <- sqrt(sum((Igray_obj - mu_omega)^2) / (M-1))
+
+# Variacion canal verde
+mu_G            <- mean(G_obj)
+variacion_verde <- sqrt(sum((G_obj - mu_G)^2) / (M-1))
+
+# PASO 5: Ver resultados de la imagen de prueba
+cat("Brillo medio:   ", brillo_medio, "\n")
+cat("Contraste:      ", contraste, "\n")
+cat("Area:           ", area, "\n")
+cat("Rugosidad:      ", rugosidad, "\n")
+cat("Variacion verde:", variacion_verde, "\n")
 
 
 
